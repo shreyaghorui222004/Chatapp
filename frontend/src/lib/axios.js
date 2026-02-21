7e658dabadeb5193432718ef0880fb6fc5833c02
Shreya Ghorui <shreyaghorui202@gmail.com>
2026-02-21 20:58:10 +0530

    Use VITE_API_URL properly

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
-      : "https://chatapp-x8e9.onrender.com/api",
+      : `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true,
 });
