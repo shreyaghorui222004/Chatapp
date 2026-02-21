e2a382a3831a83e0be98875c478ea06422a66ef2
Shreya Ghorui <shreyaghorui202@gmail.com>
2026-02-21 21:40:22 +0530

    Monolithic deployment setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
 import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
@@ -8,35 +9,29 @@ import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 import { app, server } from "./lib/socket.js";
 
+const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
-
-/* ==============================
-   MIDDLEWARE
-================================ */
 
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://chatapp-gray-three.vercel.app",
-    ],
-    credentials: true,
-  })
-);
-
-/* ==============================
-   ROUTES
-================================ */
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-/* ==============================
-   START SERVER
+/* ===============================
+   SERVE FRONTEND BUILD
 ================================ */
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 
 server.listen(PORT, () => {
   console.log("Server running on port: " + PORT);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:3000/api"
-      : `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: "/api",
   withCredentials: true,
 });
