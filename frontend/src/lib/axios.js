9fad0c6e0f1524465c1418cf3db5be6af5e9b8dc
Shreya Ghorui <shreyaghorui202@gmail.com>
2026-02-21 20:26:21 +0530

    Fix production API baseURL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:3000/api"
-    : "https://chatapp-x8e9.onrender.com/api";
-
 export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3000/api"
+      : "https://chatapp-x8e9.onrender.com/api",
   withCredentials: true,
 });
